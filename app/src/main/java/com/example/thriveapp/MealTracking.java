05b1ca9ff6fc6d91e9f6f75cf8cc4cafdf3c5d58
jynf <jacobzimmerhanzel3@gmail.com>
2025-04-16 00:40:14 -0500

    Update MealTracking.java

--- app/src/main/java/com/example/thriveapp/MealTracking.java
+++ app/src/main/java/com/example/thriveapp/MealTracking.java
@@ -19,6 +19,9 @@ import com.google.android.material.tabs.TabLayout;
 import java.util.Objects;
 
 public class MealTracking extends AppCompatActivity {
+    private DatabaseMealHelper dbMealHelper = new DatabaseMealHelper(this);
+
+    private boolean loaded = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +31,84 @@ public class MealTracking extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             System.out.println("workding");
-
+            if(!loaded){
+                listOfTasks();//makes list of buttons for each task
+                loaded = true;
+            }
             return insets;
         });
     }
+
+    public void ClickedNutrient(View button) {
+        String buttonText = ((Button)button).getText().toString();
+        TextView NutrientName = (TextView)findViewById(R.id.NutrientName);
+        NutrientName.setText(buttonText);
+
+
+    }
+    public void InformationAdded(View button) {
+        TextView nutrientText = (TextView)findViewById(R.id.NutrientName);
+        EditText DataField = (EditText)findViewById(R.id.dataField);
+        String taskName = nutrientText.getText().toString().trim();
+        int dataAdded =  Integer.parseInt(DataField.getText().toString());
+
+        if(!nutrientExists(taskName) || dataAdded < 0) {
+            return;}
+        int data = dataAdded;
+        dbMealHelper.addData(nutrientName, data);
+    }
+
+    public void addNutrient(View button){
+
+        EditText nutrientNameContainer = (EditText) findViewById(R.id.newNutrientNameInput);
+
+        String nutrientName = nutrientNameContainer.getText().toString();
+        if(nutrientName == "" || nutrientName == "Name") {
+            return;//empty
+        }
+        if(!dbMealHelper.addNutrient(nutrientName)) {
+            System.out.println("Saves not Succesfully");
+        }
+
+
+        Button nutrientButton = new Button(this);
+        nutrientButton.setText(nutrientName);
+        button.setBackgroundColor(Color.rgb(46,125,50));
+
+        nutrientButton.setOnClickListener(this::ClickedNutrient);
+        LinearLayout buttonContainer = (LinearLayout) findViewById(R.id.buttonContainer);
+        buttonContainer.addView(nutrientButton);
+    }
+
+    //returns true if nutrient exists
+    private boolean nutrientExists(String checknutrient){
+        String[] nutrients = dbMealHelper.getAllTrackedNutrients();
+        for(var nutrientName : nutrients) {
+            if(nutrientName.equals(checknutrient)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void listOfnutrients() {
+        System.out.println("Testing");
+        String[] nutrients = dbMealHelper.getAllTrackedNutrients();
+        for (var nutrientName : nutrients) {
+            // android:id="@+id/buttonContainer";
+            if (!Objects.equals(nutrientName, null)) {
+                LinearLayout buttonContainer = (LinearLayout) findViewById(R.id.buttonContainer);
+
+                Button button = new Button(this);
+                button.setText(nutrientName);
+                button.setBackgroundColor(Color.rgb(46, 125, 50));
+                button.setOnClickListener(this::ClickedNutrient);
+                button.setLetterSpacing(1);
+                buttonContainer.addView(button);
+            }
+        }
+
+    }
+    
+    
+    
 }
